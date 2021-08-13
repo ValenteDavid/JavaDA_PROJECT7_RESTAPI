0c772a2b2c08a1dcbeb7c09327c58c3732f0bd0d
David Valente <dvalente77@gmail.com>
2021-08-13 19:01:08 +0200

    add domain

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,76 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+  
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	Integer curveId;
+	Timestamp asOfDate;
+	Double term;
+	Double value;
+	Timestamp creationDate;
+	
+	public CurvePoint() {
+	}
+	
+	public CurvePoint(Integer curveId, Double term, Double value) {
+		this.curveId = curveId;
+		this.term = term;
+		this.value = value;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCurveId() {
+		return curveId;
+	}
+	public void setCurveId(Integer curveId) {
+		this.curveId = curveId;
+	}
+	public Timestamp getAsOfDate() {
+		return asOfDate;
+	}
+	public void setAsOfDate(Timestamp asOfDate) {
+		this.asOfDate = asOfDate;
+	}
+	public Double getTerm() {
+		return term;
+	}
+	public void setTerm(Double term) {
+		this.term = term;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+	@Override
+	public String toString() {
+		return "CurvePoint [id=" + id + ", curveId=" + curveId + ", asOfDate=" + asOfDate + ", term=" + term
+				+ ", value=" + value + ", creationDate=" + creationDate + "]";
+	}
+
 }
