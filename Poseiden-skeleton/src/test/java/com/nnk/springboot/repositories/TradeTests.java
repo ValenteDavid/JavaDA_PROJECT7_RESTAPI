f01c611dbae74b0f511a840530dd83d29d28fb77
David Valente <dvalente77@gmail.com>
2021-08-15 21:18:07 +0200

    Update TradeTests

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/repositories/TradeTests.java
@@ -0,0 +1,47 @@
+package com.nnk.springboot.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nnk.springboot.Application;
+import com.nnk.springboot.domain.Trade;
+
+@SpringBootTest(classes = Application.class)
+public class TradeTests {
+
+	@Autowired
+	private TradeRepository tradeRepository;
+
+	@Test
+	public void tradeTest() {
+		Trade trade = new Trade("Trade Account", "Type");
+
+		// Save
+		trade = tradeRepository.save(trade);
+		assertNotNull(trade.getTradeId());
+		assertTrue(trade.getAccount().equals("Trade Account"));
+
+		// Update
+		trade.setAccount("Trade Account Update");
+		trade = tradeRepository.save(trade);
+		assertTrue(trade.getAccount().equals("Trade Account Update"));
+
+		// Find
+		List<Trade> listResult = tradeRepository.findAll();
+		assertTrue(listResult.size() > 0);
+
+		// Delete
+		Integer id = trade.getTradeId();
+		tradeRepository.delete(trade);
+		Optional<Trade> tradeList = tradeRepository.findById(id);
+		assertFalse(tradeList.isPresent());
+	}
+}
