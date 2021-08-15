4842775713125f200be086b2a0f5c17a90baba65
David Valente <dvalente77@gmail.com>
2021-08-16 01:58:17 +0200

    Update UserController and add test

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -1,9 +1,8 @@
 package com.nnk.springboot.controllers;
 
-import com.nnk.springboot.domain.User;
-import com.nnk.springboot.repositories.UserRepository;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
 
 @Controller
 public class UserController {
@@ -34,8 +34,8 @@ public class UserController {
     @PostMapping("/user/validate")
     public String validate(@Valid User user, BindingResult result, Model model) {
         if (!result.hasErrors()) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode(user.getPassword()));
+//            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//            user.setPassword(encoder.encode(user.getPassword()));
             userRepository.save(user);
             model.addAttribute("users", userRepository.findAll());
             return "redirect:/user/list";
@@ -58,8 +58,8 @@ public class UserController {
             return "user/update";
         }
 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        user.setPassword(encoder.encode(user.getPassword()));
         user.setId(id);
         userRepository.save(user);
         model.addAttribute("users", userRepository.findAll());
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/UserControllerTest.java
@@ -0,0 +1,191 @@
+package com.nnk.springboot.controllers;
+
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+
+	@MockBean
+	private UserRepository userRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void HomeTest() throws Exception {
+		mockMvc.perform(get("/user/list"))
+				.andExpect(model().attributeExists("users"))
+				.andExpect(view().name("user/list"));
+	}
+
+	@Test
+	public void addUserTest() throws Exception {
+		mockMvc.perform(get("/user/add"))
+				.andExpect(view().name("user/add"));
+	}
+
+	@Test
+	public void validateTest() throws Exception {
+		User user = new User("User Name", "Password","Full Name", "Role");
+		when(userRepository.save(user)).thenReturn(user);
+		when(userRepository.findAll()).thenReturn(anyList());
+
+		mockMvc.perform(post("/user/validate")
+				.param("username", "User Name")
+				.param("password", "Password")
+				.param("fullname", "Full Name")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("redirect:/user/list"));
+	}
+	
+	@Test
+	public void validate_EmptyUsername_Test() throws Exception {
+		mockMvc.perform(post("/user/validate")
+				.param("username", "")
+				.param("password", "Password")
+				.param("fullname", "Full Name")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("user/add"));
+	}
+	
+	@Test
+	public void validate_EmptyPassword_Test() throws Exception {
+		mockMvc.perform(post("/user/validate")
+				.param("username", "User Name")
+				.param("password", "")
+				.param("fullname", "Full Name")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("user/add"));
+	}
+	
+	@Test
+	public void validate_EmptyFullname_Test() throws Exception {
+		mockMvc.perform(post("/user/validate")
+				.param("username", "User Name")
+				.param("password", "Password")
+				.param("fullname", "")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("user/add"));
+	}
+	
+	@Test
+	public void validate_EmptyRole_Test() throws Exception {
+		mockMvc.perform(post("/user/validate")
+				.param("username", "User Name")
+				.param("password", "Password")
+				.param("fullname", "Full Name")
+				.param("role", "")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("user/add"));
+	}
+
+
+	@Test
+	public void showUpdateFormTest() throws Exception {
+		Integer id = 1;
+		when(userRepository.findById(id)).thenReturn(
+				Optional.of(new User ("User Name", "Password","Full Name", "Role")));
+
+		mockMvc.perform(get("/user/update/{0}", id))
+				.andExpect(model().attributeExists("user"))
+				.andExpect(view().name("user/update"));
+	}
+
+	@Test
+	public void showUpdateForm_NotFoundId_Test() throws Exception {
+		Integer id = 1;
+		when(userRepository.findById(id)).thenReturn(Optional.empty());
+
+		Assertions
+				.assertThatThrownBy(() -> mockMvc.perform(get("/user/update/{0}", id)))
+				.hasCause(new IllegalArgumentException("Invalid user Id:" + id));
+	}
+
+
+	@Test
+	public void updateUser_Test() throws Exception {
+		Integer id = 1;
+		User user = new User("UserName", "Password","Full Name", "Role");
+		user.setId(id);
+		when(userRepository.save(user)).thenReturn(user);
+		when(userRepository.findAll()).thenReturn(anyList());
+
+		mockMvc.perform(post("/user/update/{id}", id)
+				.param("id", "1")
+				.param("username", "UserName")
+				.param("password", "Password")
+				.param("fullname", "Full Name")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("redirect:/user/list"));
+	}
+
+	@Test
+	public void updateUser_Test_HasError() throws Exception {
+		Integer id = 1;
+
+		mockMvc.perform(post("/user/update/{id}", id)
+				.param("id", "1")
+				.param("username", "")
+				.param("password", "Password")
+				.param("fullname", "Full Name")
+				.param("role", "Role")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(view().name("user/update"));
+	}
+
+	@Test
+	public void deleteTest() throws Exception {
+		Integer id = 1;
+		when(userRepository.findById(id)).thenReturn(
+				Optional.of(new User("UserName", "Password","Full Name", "Role")));
+		when(userRepository.findAll()).thenReturn(anyList());
+
+		mockMvc.perform(get("/user/delete/{0}", id))
+				.andExpect(view().name("redirect:/user/list"));
+
+	}
+
+	@Test
+	public void deleteTest_NotFoundId_() throws Exception {
+		Integer id = 1;
+		when(userRepository.findById(id)).thenReturn(Optional.empty());
+		when(userRepository.findAll()).thenReturn(anyList());
+
+		Assertions
+				.assertThatThrownBy(() -> mockMvc.perform(get("/user/delete/{0}", id)))
+				.hasCause(new IllegalArgumentException("Invalid user Id:" + id));
+
+	}
+
+
+}
