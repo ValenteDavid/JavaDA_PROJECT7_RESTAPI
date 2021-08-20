6e82297222a16a74a29730a7b92ac6fd5538f180
David Valente <dvalente77@gmail.com>
2021-08-20 17:40:23 +0200

    Update Spring Security

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -1,39 +1,53 @@
 package com.nnk.springboot.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.jdbcAuthentication()
-		auth.inMemoryAuthentication()
-		.withUser("springuser").password(passwordEncoder().encode("spring123")).roles("USER")
-		.and()
-		.withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ADMIN","USER");
+		 auth.jdbcAuthentication().dataSource(dataSource)
+         .usersByUsernameQuery(
+                 "SELECT username, password,'true' as enabled FROM users WHERE username=?")
+         .authoritiesByUsernameQuery(
+                 "SELECT username, role FROM users WHERE username=?");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/admin").hasRole("ADMIN" )
 		.antMatchers("/user").hasRole("USER")
 		.anyRequest().authenticated()
 		.and()
 		.formLogin();
 	}
-
+	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	GrantedAuthorityDefaults grantedAuthorityDefaults() {
+	    return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+	}
+	
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
